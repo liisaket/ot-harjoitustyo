0d81e6b4d0159cc621eab3b4017f09dc13abf21e
Liisa <liisa-m@outlook.com>
2022-11-10 15:20:21 +0200

    Unicafe; teht. 6

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -7,3 +7,19 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+    
+    # Vk2 Teht. 6
+    def test_konstruktori_asettaa_saldon_oikein(self):
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.00 euroa")
+    
+    def test_kortille_voi_ladata_rahaa(self):
+        self.maksukortti.lataa_rahaa(1000)
+
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 20.00 euroa")
+    
+    def test_rahan_ottaminen_toimii(self):
+        x = self.maksukortti.ota_rahaa(500)
+        self.assertTrue(x, "Kortilla on rahaa 5.00 euroa")
+        
+        y = self.maksukortti.ota_rahaa(600)
+        self.assertFalse(y, "Kortilla on rahaa 5.00 euroa")
