607e9819579037b70f0844442978bdea239e4995
Liisa <liisa-m@outlook.com>
2022-11-21 22:00:42 +0200

    add repositories/

---
+++ src/repositories/user_repository.py
@@ -0,0 +1,17 @@
+class UserRepository:
+    def __init__(self, connection):
+        self._connection = connection
+
+    def find_all(self):
+        cursor = self._connection.cursor()
+
+        cursor.execute("select * from users")
+
+        rows = cursor.fetchall()
+
+        return [User(row["username"], row["password"]) for row in rows]
+
+
+user_repository = UserRepository(get_database_connection())
+users = user_repository.find_all()
+
