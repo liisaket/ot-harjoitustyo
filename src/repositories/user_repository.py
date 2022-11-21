eb66a5296a62964bb33a449954fe00ff9030bd1f
Liisa <liisa-m@outlook.com>
2022-11-21 23:29:26 +0200

    add user_repository

--- src/repositories/user_repository.py
+++ src/repositories/user_repository.py
@@ -1,6 +1,21 @@
+from entities.user import User
+from database_connection import get_database_connection
+
 class UserRepository:
     def __init__(self, connection):
         self._connection = connection
+    
+    def create(self, user):
+        cursor = self._connection.cursor()
+
+        cursor.execute(
+            "insert into users (username, password) values (?, ?)",
+            (user.username, user.password)
+        )
+
+        self._connection.commit()
+
+        return user
 
     def find_all(self):
         cursor = self._connection.cursor()
@@ -10,8 +25,14 @@ class UserRepository:
         rows = cursor.fetchall()
 
         return [User(row["username"], row["password"]) for row in rows]
+    
+    def delete_all(self):
+        cursor = self._connection.cursor()
+
+        cursor.execute("delete from users")
+
+        self._connection.commit()
 
 
 user_repository = UserRepository(get_database_connection())
-users = user_repository.find_all()
 
