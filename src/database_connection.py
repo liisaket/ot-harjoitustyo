290969a71ed25b22d95109c4d45427ad728c8aad
Liisa <liisa-m@outlook.com>
2022-11-21 23:28:05 +0200

    database connection

--- src/database_connection.py
+++ src/database_connection.py
@@ -1,11 +1,8 @@
-import os
 import sqlite3
+from config import DATABASE_FILE_PATH
 
-dirname = os.path.dirname(__file__)
-
-connection = sqlite3.connect(os.path.join(dirname, "..", "data", "database.sqlite"))
+connection = sqlite3.connect(DATABASE_FILE_PATH)
 connection.row_factory = sqlite3.Row
-
 
 def get_database_connection():
     return connection
