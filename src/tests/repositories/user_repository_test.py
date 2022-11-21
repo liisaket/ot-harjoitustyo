892456610cc38d7c776bc8f9f769aec5d107d593
Liisa <liisa-m@outlook.com>
2022-11-21 23:31:47 +0200

    add user_repo_test

---
+++ src/tests/repositories/user_repository_test.py
@@ -0,0 +1,25 @@
+import unittest
+from repositories.user_repository import user_repository
+from entities.user import User
+
+class TestUserRepository(unittest.TestCase):
+    def setUp(self):
+        user_repository.delete_all()
+        self.user_testi = User("testi", "testi123")
+        self.user_touko = User("touko", "touko123")
+    
+    def test_create(self):
+        user_repository.create(self.user_testi)
+        users = user_repository.find_all()
+
+        self.assertEqual(len(users), 1)
+        self.assertEqual(users[0].username, self.user_testi.username)
+
+    def test_find_all(self):
+        user_repository.create(self.user_testi)
+        user_repository.create(self.user_touko)
+        users = user_repository.find_all()
+
+        self.assertEqual(len(users), 2)
+        self.assertEqual(users[0].username, self.user_testi.username)
+        self.assertEqual(users[1].username, self.user_touko.username)
