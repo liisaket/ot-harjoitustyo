a5cde6f47340767c819e689cfd311267a64d9e18
Liisa <liisa-m@outlook.com>
2022-11-22 15:04:09 +0200

    index.py

--- src/index.py
+++ src/index.py
@@ -1,5 +1,25 @@
-from tkinter import Tk
+from tkinter import Tk, ttk, constants
 
-window = Tk()
-window.title("TkInter example")
-window.mainloop()
+class UI:
+    def __init__(self, root):
+        self._root = root
+
+    def start(self):
+        heading_label = ttk.Label(master=self._root, text="Login")
+
+        username_label = ttk.Label(master=self._root, text="Username")
+        username_entry = ttk.Entry(master=self._root)
+
+        password_label = ttk.Label(master=self._root, text="Password")
+        password_entry = ttk.Entry(master=self._root)
+
+        button = ttk.Button(master=self._root, text="Button")
+
+        heading_label.grid(columnspan=2, sticky=constants.W, padx=5, pady=5)
+        username_label.grid(padx=5, pady=5)
+        username_entry.grid(row=1, column=1, sticky=(constants.E, constants.W), padx=5, pady=5)
+        password_label.grid(padx=5, pady=5)
+        password_entry.grid(row=2, column=1, sticky=(constants.E, constants.W), padx=5, pady=5)
+        button.grid(columnspan=2, sticky=(constants.E, constants.W), padx=5, pady=5)
+
+        self._root.grid_columnconfigure(1, weight=1)
