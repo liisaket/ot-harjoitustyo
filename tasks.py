deffa0f301a4d6dda0c510fbd7c3c12c281d5e41
Liisa <liisa-m@outlook.com>
2022-11-22 14:26:29 +0200

    update tasks.py

--- tasks.py
+++ tasks.py
@@ -14,7 +14,8 @@ def test(ctx):
 
 @task
 def coverage(ctx):
-    ctx.run("coverage run --branch -m pytest", pty=True)
+    ctx.run("coverage run --branch -m pytest src", pty=True)
+
 
 @task(coverage)
 def coverage_report(ctx):
