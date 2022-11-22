33122e0cdc65de234b0045035c5f4e9e36b9d1d2
Liisa <liisa-m@outlook.com>
2022-11-22 17:08:05 +0200

    add stuff

---
+++ src/services/service.py
@@ -0,0 +1,37 @@
+from entities.user import User
+
+from repositories.user_repository import (
+    user_repository as default_user_repository
+)
+
+class UsernameExistsError(Exception):
+    pass
+
+class InvalidCredentialsError(Exception):
+    pass
+
+class MtService:
+    def __init__(self,user_repository=default_user_repository):
+        self._user = None
+        self._user_repository = user_repository
+    
+    def logout(self):
+        self._user = None
+    
+    def login(self, username, password):
+        user = self._user_repository.find_by_username(username)
+        if not user or user.password != password:
+            raise InvalidCredentialsError(f"Invalid username or password")
+        self._user = user
+        return user
+    
+    def create_user(self,username,password,login=True):
+        existing_user = self._user_repository.find_by_username(username)
+        if existing_user:
+            raise UsernameExistsError(f"Username {username} already exists")
+        user = self._user_repository.create(User(username, password))
+        if login:
+            self._user = user
+        return user
+
+mtservice = MtService()
