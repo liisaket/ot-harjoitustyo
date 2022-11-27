bf825c1cc5222ee91b5f7a52de491e9c3982e2d2
Liisa <liisa-m@outlook.com>
2022-11-27 20:19:26 +0200

    ui stuff

--- src/ui/login_view.py
+++ src/ui/login_view.py
@@ -37,6 +37,22 @@ class LoginView:
     def _hide_error(self):
         self._error_label.grid_remove()
     
+    def _initialize_username_field(self):
+        username_label = ttk.Label(master=self._frame, text="Username")
+
+        self._username_entry = ttk.Entry(master=self._frame)
+
+        username_label.grid(padx=5, pady=5, sticky=constants.W)
+        self._username_entry.grid(padx=5, pady=5, sticky=constants.EW)
+
+    def _initialize_password_field(self):
+        password_label = ttk.Label(master=self._frame, text="Password")
+
+        self._password_entry = ttk.Entry(master=self._frame)
+
+        password_label.grid(padx=5, pady=5, sticky=constants.W)
+        self._password_entry.grid(padx=5, pady=5, sticky=constants.EW)
+    
     def _initialize(self):
         self._frame = ttk.Frame(master=self._root)
 
