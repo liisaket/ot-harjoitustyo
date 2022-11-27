cab60e31e50c13c467dd55bbbfa87cd17d8515bf
Liisa <liisa-m@outlook.com>
2022-11-27 20:21:00 +0200

    ui stuff

--- src/index.py
+++ src/index.py
@@ -5,8 +5,8 @@ def main():
     window = tk.Tk()
     window.title("Mood-Tracker")
     window.eval("tk::PlaceWindow . center")
-    frame1 = tk.Frame(window, width=500, height=300, bg="alice blue")
-    frame1.grid(row=0, column=0)
+    #frame1 = tk.Frame(window, width=500, height=300, bg="alice blue")
+    #frame1.grid(row=0, column=0)
 
     ui_view = UI(window)
     ui_view.start()
