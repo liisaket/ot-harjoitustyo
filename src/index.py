6c5f52d5a6a235ea5beac2295dd1c563b2793c3b
Liisa <liisa-m@outlook.com>
2022-11-27 20:28:06 +0200

    ui stuff

--- src/index.py
+++ src/index.py
@@ -4,7 +4,7 @@ from ui.ui import UI
 def main():
     window = tk.Tk()
     window.title("Mood-Tracker")
-    #window.eval("tk::PlaceWindow . center")
+    window.eval("tk::PlaceWindow . center")
     #frame1 = tk.Frame(window, width=500, height=300, bg="alice blue")
     #frame1.grid(row=0, column=0)
 
--- src/repositories/user_repository.py
+++ src/repositories/user_repository.py
@@ -7,31 +7,31 @@ class UserRepository:
     
     def create(self, user):
         cursor = self._connection.cursor()
-
         cursor.execute(
             "insert into users (username, password) values (?, ?)",
             (user.username, user.password)
         )
-
         self._connection.commit()
-
         return user
 
     def find_all(self):
         cursor = self._connection.cursor()
-
         cursor.execute("select * from users")
-
         rows = cursor.fetchall()
-
         return [User(row["username"], row["password"]) for row in rows]
+    
+    def find_by_username(self, username):
+        cursor = self._connection.cursor()
+        cursor.execute(
+            "select * from users where username = ?",
+            (username,)
+        )
+        row = cursor.fetchone()
+        return get_user_by_row(row)
     
     def delete_all(self):
         cursor = self._connection.cursor()
-
         cursor.execute("delete from users")
-
         self._connection.commit()
-
 
 user_repository = UserRepository(get_database_connection())
