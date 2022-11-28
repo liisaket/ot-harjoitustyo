de75f8bc63e1b9e0d4dbda965e763db477bdc81b
Liisa <liisa-m@outlook.com>
2022-11-28 16:36:46 +0200

    add tasks

--- tasks.py
+++ tasks.py
@@ -13,6 +13,14 @@ def test(ctx):
     ctx.run("pytest src", pty=True)
 
 @task
+def lint(ctx):
+    ctx.run("pylint src", pty=True)
+
+@task
+def format(ctx):
+    ctx.run("autopep8 --in-place --recursive src", pty=True)
+
+@task
 def coverage(ctx):
     ctx.run("coverage run --branch -m pytest src", pty=True)
 
