e90d6b78ef1b2fc2209a63c42fb98ed435bd0fcc
Liisa <liisa-m@outlook.com>
2022-11-29 17:00:56 +0200

    update entry.pu

--- src/entities/entry.py
+++ src/entities/entry.py
@@ -1,6 +1,5 @@
 import uuid
 from datetime import datetime
-from zoneinfo import ZoneInfo
 
 class Entry:
     def __init__(self, content, emotion=None, user=None, entry_id=None):
@@ -8,5 +7,4 @@ class Entry:
         self.emotion = emotion
         self.user = user
         self.id = entry_id or str(uuid.uuid4())
-        self.date = datetime.now(ZoneInfo("EET")).strftime("%Y-%m-%d %H:%M")
-
+        self.date = datetime.now().strftime("%Y-%m-%d %H:%M")
