ae066e00f5e3e64de11b8941f8e1e385607def36
liisaket <95650016+liisaket@users.noreply.github.com>
2022-12-12 17:50:36 +0200

    Update past_entries_view.py

--- src/ui/past_entries_view.py
+++ src/ui/past_entries_view.py
@@ -1,13 +1,10 @@
 from tkinter import ttk, constants
 from services.diary_service import diary_service
 
-
-class PastEntriesView:
-    def __init__(self, root, handle_logout, handle_show_main_page_view):
+class PastEntriesList:
+    def __init__(self, root, entries):
         self._root = root
-        self._handle_show_main_page_view = handle_show_main_page_view
-        self._handle_logout = handle_logout
-        self._user = diary_service.get_current_user()
+        self._entries = entries
         self._frame = None
 
         self._initialize()
@@ -17,10 +14,6 @@ class PastEntriesView:
 
     def destroy(self):
         self._frame.destroy()
-
-    def _logout_handler(self):
-        diary_service.logout()
-        self._handle_logout()
 
     def _initialize_entry_item(self, entry):
         new_line = "\n"
@@ -34,6 +27,48 @@ class PastEntriesView:
 
         item_frame.grid_columnconfigure(0, weight=1)
         item_frame.pack(fill=constants.X)
+
+    def _initialize(self):
+        self._frame = ttk.Frame(master=self._root)
+
+        for entry in self._entries:
+            self._initialize_entry_item(entry)
+
+
+class PastEntriesView:
+    def __init__(self, root, handle_logout, handle_show_main_page_view):
+        self._root = root
+        self._handle_show_main_page_view = handle_show_main_page_view
+        self._handle_logout = handle_logout
+        self._user = diary_service.get_current_user()
+        self._entries_frame = None
+        self._entries_view = None
+        self._frame = None
+
+        self._initialize()
+
+    def pack(self):
+        self._frame.pack(fill=constants.X)
+
+    def destroy(self):
+        self._frame.destroy()
+
+    def _logout_handler(self):
+        diary_service.logout()
+        self._handle_logout()
+    
+    def _initialize_entries(self):
+        if self._entries_view:
+            self._entries_view.destroy()
+
+        entries = diary_service.get_entries()
+
+        self._entries_view = PastEntriesList(
+            self._entries_frame, 
+            entries
+        )
+
+        self._entries_view.pack()
 
     def _initialize_header(self):
         user_label = ttk.Label(
@@ -58,8 +93,6 @@ class PastEntriesView:
         )
 
     def _initialize(self):
-        entries = diary_service.get_entries()
-        
         self._frame = ttk.Frame(master=self._root)
 
         past_label = ttk.Label(
@@ -68,9 +101,6 @@ class PastEntriesView:
         )
 
         past_label.grid(row=3, column=0, padx=5, pady=5, sticky=constants.W)
-        
-        for entry in entries:
-            self._initialize_entry_item(entry)
 
         go_back_button = ttk.Button(
             master=self._frame,
@@ -82,7 +112,8 @@ class PastEntriesView:
                             pady=5, sticky=constants.EW)
 
         self._frame.grid_columnconfigure(0, weight=1, minsize=400)
-
+        
+        self._initialize_entries()
         self._initialize_header()
 
         self._frame.grid_columnconfigure(0, weight=1, minsize=400)
