af61d2ebd426c265f6ca52371674ebe9f135d055
liisaket <95650016+liisaket@users.noreply.github.com>
2022-12-12 17:23:00 +0200

    Update past_entries_view.py

--- src/ui/past_entries_view.py
+++ src/ui/past_entries_view.py
@@ -21,13 +21,14 @@ class PastEntriesView:
     def _logout_handler(self):
         diary_service.logout()
         self._handle_logout()
-    
+
     def _initialize_entry_item(self, entry):
+        new_line = "\n"
         item_frame = ttk.Frame(master=self._frame)
         label = ttk.Label(
             master=item_frame, 
-            text=f"Date:{entry.date}\n
-            Emotion:{entry.emotion}\n
+            text=f"Date:{entry.date}{new_line}
+            Emotion:{entry.emotion}{new_line}
             Notes:{entry.content}"
         )
         
