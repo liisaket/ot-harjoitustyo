36465be3d90edee5989a8458c6d31aec4ae93537
liisaket <95650016+liisaket@users.noreply.github.com>
2022-12-12 15:14:32 +0200

    Update new_entry_view.py

--- src/ui/new_entry_view.py
+++ src/ui/new_entry_view.py
@@ -80,8 +80,8 @@ class NewEntryView:
             column=0,
             padx=5,
             pady=5,
-            width=15,
-            height=15,
+            width=5,
+            height=5,
             sticky=constants.EW
         )
 
