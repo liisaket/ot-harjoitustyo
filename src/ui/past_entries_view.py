fc92258a7334de18bdb12d3b106047e3b9754b2a
liisaket <95650016+liisaket@users.noreply.github.com>
2022-12-12 22:46:42 +0200

    Update past_entries_view.py

--- src/ui/past_entries_view.py
+++ src/ui/past_entries_view.py
@@ -34,7 +34,7 @@ class PastEntriesView:
         label.grid(row=4, column=0, padx=5, pady=5, sticky=constants.SW)
         item_frame.grid_columnconfigure(0, weight=1)
         item_frame.grid_columnconfigure(1, weight=0)
-        item_frame.pack(fill=constants.X, side=bottom)
+        item_frame.pack(fill=constants.X, side=BOTTOM)
 
     def _initialize_entries(self):
         entries = diary_service.get_entries()
