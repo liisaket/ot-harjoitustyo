733871f2d6935bed65ae332d5baaa8d80cfe00c6
liisaket <95650016+liisaket@users.noreply.github.com>
2022-12-12 14:59:57 +0200

    Update entry_repository.py

---
+++ src/repositories/entry_repository.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+from entities.entry import Entry
+from repositories.user_repository import user_repository
+from confid import ENTRIES_FILE_PATH
+
+class EntryRepository:
+    """Luokka, joka vastaa päiväkirjapostauksiin liittyvistä tietokantaoperaatioista."""
+    def __init__(self, file_path):
+        """Luokan konstuktori.
+        
+        Args:
+            file_apth: Polku tiedostoon, johon postaukset tallennetaan.
+        """
+
+        self._file_path = file_path
+    
+    def _ensure_file_exist(self):
+        Path(self._file_path).touch()
+    
+    def create(self, entry):
+        """Tallentaa postauksen tietokantaan.
+        
+        Args:
+            entry: Tallennettava postaus Entry-oliona.
+        Returns:
+            Tallennettu postaus Entry-oliona.
+        """
+        pass
+        
+
+
+entry_repository = EntryRepository(ENTRIES_FILE_PATH)
