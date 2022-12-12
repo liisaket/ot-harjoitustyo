1319e18bf8cd1be40801db1475155aac64168d77
liisaket <95650016+liisaket@users.noreply.github.com>
2022-12-12 23:53:34 +0200

    docstring

--- src/entities/entry.py
+++ src/entities/entry.py
@@ -3,7 +3,31 @@ from datetime import datetime
 
 
 class Entry:
-    def __init__(self, content, emotion=None, user=None, entry_id=None, date=None):
+    """Yksittäistä päiväkirjapostausta kuvaava luokka.
+    
+    Attributes:
+        content: Merkkijonoarvo, joka kuvaa käyttäjän päivää.
+        emotion: Merkkijonoarvo, joka kuvaa käyttäjän tunnetilaa.
+        user: User-olio, joka kuvaa postauksen omistajaa.
+        entry_id: Merkkijonoarvo, joka kuvaa postauksen id:tä.
+        date: Merkkijonoarvo, joka kuvaa postauksen luomispäivämäärää.
+    """
+    def __init__(self, content, emotion, user=None, entry_id=None, date=None):
+        """Luokan konstruktori. Luo uuden postauksen.
+        
+        Args:
+            content: Merkkijonoarvo, joka kuvaa käyttäjän päivää.
+            emotion: Merkkijonoarvo, joka kuvaa käyttäjän tunnetilaa.
+            user:
+                Vapaaehtoinen, oletusarvoltaan None.
+                User-olio, joka kuvaa postauksen omistajaa.
+            entry_id:
+                Vapaaehtoinen, oletusarvoltaan generoitu uuid.
+                Merkkijonoarvo, joka kuvaa postauksen id:tä.
+            date:
+                Vapaaehtoinen, oletusarvoltaan generoitu datetime-olio.
+                Merkkijonoarvo, joka kuvaa postauksen luomispäivämäärää. 
+        """
         self.content = content
         self.emotion = emotion
         self.user = user
