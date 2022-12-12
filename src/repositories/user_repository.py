de31e4efec25ce5324e36c02e4d03626c692a5c0
liisaket <95650016+liisaket@users.noreply.github.com>
2022-12-13 00:37:14 +0200

    docstring

--- src/repositories/user_repository.py
+++ src/repositories/user_repository.py
@@ -3,10 +3,25 @@ from database_connection import get_database_connection
 
 
 class UserRepository:
+    """Luokka, joka vastaa käyttäjiin liittyvistä tietokantaoperaatioista."""
     def __init__(self, connection):
+        """Luokan konstruktori.
+        
+        Args:
+            connection = Tietokantayhteyden Connection-olio
+        """
+        
         self._connection = connection
 
     def create(self, user):
+        """Tallentaa käyttäjän tietokantaan.
+        
+        Args:
+            user: Tallennettava käyttäjä User-oliona.
+        Returns:
+            Palauttaa tallennetun käyttäjän User-oliona.
+        """
+        
         cursor = self._connection.cursor()
         cursor.execute(
             "insert into users (username, password) values (?, ?)",
@@ -16,12 +31,27 @@ class UserRepository:
         return user
 
     def find_all(self):
+        """Palauttaa kaikki käyttäjät.
+        
+        Returns:
+            Palauttaa listan kaikista käyttäjistä User-olioina.
+        """
+        
         cursor = self._connection.cursor()
         cursor.execute("select * from users")
         rows = cursor.fetchall()
         return [User(row["username"], row["password"]) for row in rows]
 
     def find_by_username(self, username):
+        """Palauttaa käyttäjän käyttäjätunnuksen perusteella.
+        
+        Args:
+            username: Käyttäjätunnus, jonka perusteella käyttäjä haetaan.
+        Returns:
+            Palauttaa käyttäjän User-oliona, jos haettu käyttäjätunnus löytyy tietokannasta.
+            Muussa tapauksessa palauttaa None.
+        """
+        
         cursor = self._connection.cursor()
         cursor.execute(
             "select * from users where username = ?",
@@ -31,6 +61,7 @@ class UserRepository:
         return User(row["username"], row["password"]) if row else None
 
     def delete_all(self):
+        """Poistaa kaikki käyttäjät tietokannasta."""
         cursor = self._connection.cursor()
         cursor.execute("delete from users")
         self._connection.commit()
