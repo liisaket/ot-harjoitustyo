561b6dadc19db2e9dc8ec599251f1a45407950ab
liisaket <95650016+liisaket@users.noreply.github.com>
2022-12-12 16:31:49 +0200

    Update entry_repository.py

--- src/repositories/entry_repository.py
+++ src/repositories/entry_repository.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 from entities.entry import Entry
 from repositories.user_repository import user_repository
-from config import ENTRIES_FILE_PATH
+from confid import ENTRIES_FILE_PATH
 
 class EntryRepository:
     """Luokka, joka vastaa päiväkirjapostauksiin liittyvistä tietokantaoperaatioista."""
@@ -25,8 +25,45 @@ class EntryRepository:
         Returns:
             Tallennettu postaus Entry-oliona.
         """
-        pass
         
+        entries = self.find_all()
+        entries.append(entry)
+        self._write(entries)
+
+        return entry
+    
+    def _read(self):
+        entries = []
+        self._ensure_file_exist()
+
+        with open(self._file_path, encoding="utf-8") as file:
+            for row in file:
+                row = row.replace("\n", "")
+                parts = row.split(";")
+
+                entry_id = parts[0]
+                date = parts[1]
+                content = parts[2]
+                emotion = parts[3]
+                username = parts[4]
+
+                user = user_repository.find_by_username(
+                    username) if username else None
+                
+                entries.append(
+                    Entry(content, emotion, user, entry_id, date)
+                )
+        
+        return entries
+    
+    def _write(self, entries):
+        self._ensure_file_exist()
+
+        with open(self._file_path, "w", encoding="utf-8") as file:
+            for entry in entries:
+                username =  entry.user.username if entry.user else ""
+                row = f"{entry.id};{entry.date};{entry.content};{entry.emotion};{username}"
+                file.write(row+"\n")
 
 
 entry_repository = EntryRepository(ENTRIES_FILE_PATH)
