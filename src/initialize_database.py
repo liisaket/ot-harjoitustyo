376994633935bf244e1d020e6859cefc8a19f315
liisaket <95650016+liisaket@users.noreply.github.com>
2022-12-13 20:42:29 +0200

    docstring

--- src/initialize_database.py
+++ src/initialize_database.py
@@ -2,6 +2,12 @@ from database_connection import get_database_connection
 
 
 def drop_tables(connection):
+    """Poistaa tietokantataulut.
+    
+    Args:
+        connection: Tietokantayhteyden Connection-olio
+    """
+    
     cursor = connection.cursor()
 
     cursor.execute('''
@@ -12,6 +18,12 @@ def drop_tables(connection):
 
 
 def create_tables(connection):
+    """Luo tietokantataulut.
+   
+    Args:
+        connection: Tietokantayhteyden Connection-olio
+    """
+
     cursor = connection.cursor()
 
     cursor.execute('''
@@ -25,6 +37,8 @@ def create_tables(connection):
 
 
 def initialize_database():
+    """Alustaa tietokantataulut."""
+    
     connection = get_database_connection()
 
     drop_tables(connection)
