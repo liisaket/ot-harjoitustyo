ed55193a43a033f85ecb0213ff761bf7a5bb7ed0
liisaket <95650016+liisaket@users.noreply.github.com>
2022-12-23 18:00:36 +0200

    Update entry.py

--- src/entities/entry.py
+++ src/entities/entry.py
@@ -29,7 +29,6 @@ class Entry:
                 Vapaaehtoinen, oletusarvoltaan generoitu datetime-olio.
                 Merkkijonoarvo, joka kuvaa postauksen luomispäivämäärää.
         """
-        
         self.content = content
         self.emotion = emotion
         self.user = user
